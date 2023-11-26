0ee08f4ff88355c9ff86e2d51b57965044a334dd
joshua gibson <bosco@joshuas-MacBook-Pro.local>
2023-11-25 22:50:13 -0500

    added lizard guess game

--- cat.js
+++ cat.js
@@ -1,3 +1,4 @@
+//sticky navbar
 var navbar = document.getElementById("navbar");
 var menu = document.getElementById("menu");
 
@@ -9,8 +10,41 @@ window.onscroll = function(){
         navbar.classList.remove("sticky");
     }
 } 
-      
-      
+
+//lizard guessing game
+const targetNumber = 23;
+let attempts = 0;
+const maxAttempts = 5;
+
+function checkGuess() {
+    const userGuess = parseInt(document.getElementById("guess").value);
+    const messageElement = document.getElementById("message");
+
+    if (isNaN(userGuess) || userGuess < 1 || userGuess > 50) {
+        setMessage("We've had somewhere between 1-50 sightings. How many lizards is too many lizards according to the health department?");
+    } else {
+        attempts++;
+
+        if (userGuess === targetNumber) {
+            setMessage(`Congratulations! You guessed it, 23 lizards. Which isn't even that many if you think about it. And that's 23 "reported incidents" so who's to say some of those weren't the same lizard.`);
+            document.getElementById("guessButton").disabled = true;
+        } else {
+            if (attempts >= maxAttempts) {
+                setMessage(`Sorry, you ran out of tries. The correct number was ${targetNumber} but who's counting?...Besides the health department...`);
+                document.getElementById("guessButton").disabled = true;
+            } else {
+                setMessage(userGuess < targetNumber ? "Higher!" : "Lower!");
+            }
+        }
+    }
+}
+
+function setMessage(message) {
+    document.getElementById("message").textContent = message;
+}
+
+
+//cat match quiz
       const quizData = [
             {
                 question: "Which best describes how much hair your cat should have?",
